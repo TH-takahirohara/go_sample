3c38ba80f01af42cfbbf49989ca8b0fd2177cbaf
Takahiro Hara <takahiro.hb@gmail.com>
2022-12-25 16:31:33 +0900

    update chi_sample
    テスト実行時に正しいリクエスト先URLが表示されるように処理を変更

--- chi_sample/main_test.go
+++ chi_sample/main_test.go
@@ -9,25 +9,28 @@ import (
 )
 
 func TestNewRouter(t *testing.T) {
+	// routerの作成
 	mux := NewRouter(context.Background())
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest(http.MethodGet, "/test/100", nil)
+	// 上で作成したrouterで、テスト用サーバーを起動
+	ts := httptest.NewServer(mux)
+	// テスト用リクエストの作成
+	r, err := http.NewRequest(http.MethodGet, ts.URL+"/test/100", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
+	want := "testId is 100"
 
-	mux.ServeHTTP(w, r)
-	resp := w.Result()
+	// リクエストの実行、レスポンスの取得
+	resp, err := http.DefaultClient.Do(r)
 	if err != nil {
 		t.Fatal(err)
 	}
+	// レスポンスボディの読み取り
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer resp.Body.Close()
-
-	want := "testId is 100"
 
 	if string(respBody) != want {
 		t.Errorf("got %q, want %q", respBody, want)
